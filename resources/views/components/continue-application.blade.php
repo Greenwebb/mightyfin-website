da0e635eca34619dbb00f0537f197a9ab1bc5b95
unknown <theegreenweb@gmail.com>
2023-12-21 09:34:12 +0200

    Admin UI Merging processes

--- resources/views/components/continue-application.blade.php
+++ resources/views/components/continue-application.blade.php
@@ -599,7 +599,7 @@
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script>
     AOS.init();
-    // let currentStep = 1;
+    let currentStep = 1;
     showStep(1);
 
     function showStep(step) {
